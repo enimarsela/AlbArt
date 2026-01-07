fc787b51b26cb137cceb29bb4b4acb8f940c6620
Airin Shestani <airinshestani@gmail.com>
2026-01-07 14:39:05 +0100

    Shtimi i Repository, Service, Controller per Produktin

--- albart/src/main/java/repository/ProduktiRepository.java
+++ albart/src/main/java/repository/ProduktiRepository.java
@@ -3,5 +3,9 @@ package repository;
 import models.Produkti;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProduktiRepository extends JpaRepository<Produkti, Long> {
+    public List<Produkti> findProduktiByKategoria(Long kategoriaId);
+    public List<Produkti> findProduktiByArtisti(Long artistiId);
 }
